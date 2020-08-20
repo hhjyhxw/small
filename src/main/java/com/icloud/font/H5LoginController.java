f8e035a23bc30e465579a24d67295071fe7d1ba0
hhjyhxw <512162086@qq.com>
2020-08-20 16:24:17 +0800

    add

--- src/main/java/com/icloud/font/H5LoginController.java
+++ src/main/java/com/icloud/font/H5LoginController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.Map;
 
 /**
  * 前后端分离之h5登陆
@@ -33,24 +32,27 @@ public class H5LoginController {
     private RedisService redisService;
 
     @RequestMapping("/login")
-    public String login(String fromType){
-        log.info("fromType=="+fromType);
-        if(!StringUtil.checkStr(fromType)){
-            log.info("fromType不能为空");
+    public String login(String redirectUrl){
+        log.info("redirectUrl=="+redirectUrl);
+        if(!StringUtil.checkStr(redirectUrl)){
+            log.info("redirectUrl不能为空");
             return "modules/h5login/error";//
         }
         try {
             WxUser user = (WxUser)request.getSession().getAttribute("wx_user");
-            Map<String,String> map = myPropertitys.getThirdloginUrlMap();
-            if(!map.containsKey(fromType)){
-                log.info("未授权的请求地址");
-                return "modules/h5login/error";//
-            }
+//            Map<String,String> map = myPropertitys.getThirdloginUrlMap();
+//            if(!map.containsKey(fromType)){
+//                log.info("未授权的请求地址");
+//                return "modules/h5login/error";//
+//            }
             String h5token = new RandomGenerator(12).generate();
             redisService.set(MD5Utils.encode2hex(h5token),user,3000L);//兼容h5、APP 前端服务 登陆
-
-            String redirectUrl = map.get(fromType).toString();
             log.info("redirectUrl=="+ redirectUrl);
+            if(redirectUrl.indexOf("?")>=0){
+                redirectUrl = redirectUrl+"&token="+h5token;
+            }else{
+                redirectUrl = redirectUrl+"?token="+h5token;
+            }
             response.sendRedirect(redirectUrl+"?token="+ h5token);
             return null;
         }catch (Exception e){
