02844eea52e6146fe9e6696b79e09224a7953dc8
hhjyhxw <512162086@qq.com>
2020-10-22 17:24:43 +0800

    add

---
+++ src/main/java/com/icloud/api/small/CarCallBackController.java
@@ -0,0 +1,78 @@
+package com.icloud.api.small;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.icloud.annotation.AuthIgnore;
+import com.icloud.annotation.LoginUser;
+import com.icloud.basecommon.util.codec.AesUtils;
+import com.icloud.common.R;
+import com.icloud.common.validator.ValidatorUtils;
+import com.icloud.config.global.MyPropertitys;
+import com.icloud.modules.bsactivity.service.BsactivityAdService;
+import com.icloud.modules.small.entity.SmallCart;
+import com.icloud.modules.small.entity.SmallRececardRecord;
+import com.icloud.modules.small.entity.SmallSpu;
+import com.icloud.modules.small.service.SmallCartService;
+import com.icloud.modules.small.service.SmallRececardRecordService;
+import com.icloud.modules.small.service.SmallSpuService;
+import com.icloud.modules.small.util.CartOrderUtil;
+import com.icloud.modules.small.vo.CartTotalVo;
+import com.icloud.modules.small.vo.CartVo;
+import com.icloud.modules.small.vo.card.CardReceiveParamVo;
+import com.icloud.modules.wx.entity.WxUser;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.CollectionUtils;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Api("卡券核销数据回推接口")
+@RestController
+@RequestMapping("/api/card")
+public class CarCallBackController {
+
+    @Autowired
+    private BsactivityAdService bsactivityAdService;
+    @Autowired
+    private SmallCartService smallCartService;
+    @Autowired
+    private SmallSpuService smallSpuService;
+    @Autowired
+    private MyPropertitys myPropertitys;
+    @Autowired
+    private SmallRececardRecordService smallRececardRecordService;
+    /**
+     * 卡券核销数据回推接口
+     * @return
+     */
+    @ApiOperation(value="卡券核销数据回推接口", notes="")
+    @RequestMapping(value = "/cartList",method = {RequestMethod.POST})
+    @ResponseBody
+    @AuthIgnore
+    public R cartList(@RequestBody CardReceiveParamVo verifycarvo) {
+        ValidatorUtils.validateEntityForFront(verifycarvo);
+        String aeskey =  myPropertitys.getCard().getAeskey();
+        String cardCode = AesUtils.encode(verifycarvo.getData(),aeskey);
+       List<SmallRececardRecord> list = smallRececardRecordService.list(new QueryWrapper<SmallRececardRecord>().eq("order_no",cardCode));
+       if(list!=null && list.size()>0){
+           //核销状态(0未核销 1核销中 2已核销 3核销失败)
+           SmallRececardRecord smallRececardRecord = list.get(0);
+           smallRececardRecord.setVerifyStatus(2);
+           boolean reuslt = smallRececardRecordService.updateById(smallRececardRecord);
+           if(reuslt){
+               return R.ok();
+           }else{
+               return R.error("核销失败");
+           }
+       }
+       //查询不到返回成功
+        return R.ok();
+    }
+
+}
--- src/main/java/com/icloud/modules/small/vo/card/CardReceiveParamVo.java
+++ src/main/java/com/icloud/modules/small/vo/card/CardReceiveParamVo.java
@@ -1,9 +1,14 @@
 package com.icloud.modules.small.vo.card;
 
 import lombok.Data;
+import lombok.NonNull;
+
+import javax.validation.constraints.NotEmpty;
 
 @Data
 public class CardReceiveParamVo {
+    @NotEmpty(message = "data数据不能为空")
     private String data;//业务数据加密窜
+    @NotEmpty(message = "渠道号不能为空")
     private String channelCode;//渠道号
 }
